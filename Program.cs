909490aa3bceee5247940d7ad9ef596786d2c8a5
user <user@WIN-QM0DGDQ42DL>
2024-09-23 18:49:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿float one, two, result;
+char sign;
+Console.Write("Введите первое число: ");
+one  = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите операцию: ");
+sign = Convert.ToChar(Console.ReadLine());
+Console.Write("Введите второе число: ");
+two = Convert.ToSingle(Console.ReadLine());
+
+if (sign == '+')
+{
+    result = one + two;
+    Console.WriteLine($"{one} {sign} {two} = {result}");
+}
+else if (sign == '-')
+{
+    result = one - two;
+    Console.WriteLine($"{one} {sign} {two} = {result}");
+}
+else if (sign == '*')
+{
+    result = one * two;
+    Console.WriteLine($"{one} {sign} {two} = {result}");
+}
+else if (sign == '/')
+{
+    if (two == 0)
+    {
+        Console.WriteLine("Ошибка. На ноль делить нельзя");
+    }
+    else if (sign == '/')
+    {
+        result = one / two;
+        Console.WriteLine($"{one} {sign} {two} = {result}");
+
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка. Данная операция не поддерживается");
+}
